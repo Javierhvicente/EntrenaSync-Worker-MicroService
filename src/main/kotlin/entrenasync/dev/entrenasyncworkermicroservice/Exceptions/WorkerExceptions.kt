fd78f8780ba41b45f3aed0b72517be9ac0702320
Javier Hernández vicente <javier.hernandez@alumno.iesluisvives.org>
2025-03-17 20:04:15 +0100

    Global exception handler added

--- src/main/kotlin/entrenasync/dev/entrenasyncworkermicroservice/Exceptions/WorkerExceptions.kt
+++ src/main/kotlin/entrenasync/dev/entrenasyncworkermicroservice/Exceptions/WorkerExceptions.kt
@@ -2,8 +2,10 @@ package entrenasync.dev.entrenasyncworkermicroservice.Exceptions
 
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
-
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 sealed class WorkerExceptions(message: String): Exception(message) {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     class WorkerNotFound(id: String): WorkerExceptions("Worker not found with id: $id")
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     class WorkerNotFoundWithName(name: String): WorkerExceptions("Worker not found with name: $name")
 }
